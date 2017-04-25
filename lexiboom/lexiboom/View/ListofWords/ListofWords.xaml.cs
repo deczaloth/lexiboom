006fa308b1bbf50117b9a833889a1afada3adaf3
deczaloth <julianavila.juarez@gmail.com>
2017-04-25 23:03:13 +0200

    DoubleTap recognizer added to a cell view
    Doubletap recognizer was added succesfully to a TextCell in order to
    allow a call on edit when a row is double tapped.

--- lexiboom/lexiboom/View/ListofWords/ListofWords.xaml.cs
+++ lexiboom/lexiboom/View/ListofWords/ListofWords.xaml.cs
@@ -4,12 +4,16 @@ using System.Collections.ObjectModel;
 using lexiboom.ViewModel.ListofWordsViewModel;
 
 using Xamarin.Forms;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace lexiboom.View.ListofWords
-{
+{    
 	public partial class ListofWords : ContentPage
 	{
-		public ListofWords ()
+        bool flag = false;
+        int counter = 0;
+        public ListofWords ()
 		{
             BindingContext = App.MotherTonge;
 			InitializeComponent ();
@@ -19,5 +23,31 @@ namespace lexiboom.View.ListofWords
         {
             await Navigation.PushAsync(new ListofWordsAddWord());
         }
-	}
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            var item = e.SelectedItem;
+        }
+
+        public async void TextCell_Tapped(object sender, EventArgs e)
+        {
+            counter++;
+            await Task.Run(async () =>
+            {
+                await Task.Delay(500);
+                if (counter > 1)
+                {
+                    counter = 0;
+                    flag = true;
+                }
+                counter = 0;
+                });
+            if (flag==true)
+            {
+                flag = false;
+                await Navigation.PushAsync(new ListofWordsEditWord());
+            }
+        }
+    }
 }
---
+++ lexiboom/lexiboom/View/ListofWords/ListofWordsEditWord.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lexiboom.View.ListofWords
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListofWordsEditWord : ContentPage
+	{
+		public ListofWordsEditWord ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
