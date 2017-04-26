c5ccc6f8e223f0e67c4f2be7572f138ec808795c
deczaloth <julianavila.juarez@gmail.com>
2017-04-26 22:33:30 +0200

    XAMLC
    Added on App

--- lexiboom/lexiboom/View/ListofWords/ListofWordsEditWord.xaml.cs
+++ lexiboom/lexiboom/View/ListofWords/ListofWordsEditWord.xaml.cs
@@ -9,7 +9,6 @@ using Xamarin.Forms.Xaml;
 
 namespace lexiboom.View.ListofWords
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListofWordsEditWord : ContentPage
 	{
 		public ListofWordsEditWord ()
